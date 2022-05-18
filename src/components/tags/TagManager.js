97d0e6be829a72c211951a50544f0efef3d9a9af
Christian Haggerty <captivationstation@gmail.com>
2022-05-18 09:37:59 -0500

    auth and view all tags

--- src/components/tags/TagManager.js
+++ src/components/tags/TagManager.js
@@ -3,6 +3,10 @@
 const API = 'http://localhost:8000'
 
 export const getAllTags = () => {
-  return fetch(`${API}/tags`)
+  return fetch(`${API}/tags`, {
+    headers:{
+      "Authorization": `Token ${localStorage.getItem("token")}`
+    }
+  })
     .then((res) => res.json())
 }
